4683da605db60391248ab92e82146906cc0e718a
Adhikari <vish.dba1@gmail.com>
2016-02-21 15:54:22 +0000

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | VishApp"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | VishApp"
   end
-
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | VishApp"
+  end
 end
