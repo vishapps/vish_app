8e2368a71e107d39cf6aed6242f55818ba93a966
Adhikari <vish.dba1@gmail.com>
2016-02-21 22:36:06 +0000

    add a full title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | VishApp"
+    assert_select "title", "VishApp"
   end
 
   test "should get help" do
