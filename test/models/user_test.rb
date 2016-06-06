2e35d174879d2dc4c6f57c160043f1a91e13295e
Adhikari <vish.dba1@gmail.com>
2016-06-06 18:12:14 +0000

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,10 +18,39 @@ class UserTest < ActiveSupport::TestCase
   end
   
     test "associated microposts should be destroyed" do
-    @user.save
-    @user.microposts.create!(content: "Lorem ipsum")
-    assert_difference 'Micropost.count', -1 do
+      @user.save
+      @user.microposts.create!(content: "Lorem ipsum")
+      assert_difference 'Micropost.count', -1 do
       @user.destroy
-      end
     end
+      
+    test "should follow and unfollow a user" do
+      vishwa = users(:vishwa)
+      archer  = users(:archer)
+      assert_not vishwa.following?(archer)
+      vishwa.follow(archer)
+      assert vishwa.following?(archer)
+      assert archer.followers.include?(vishwa)
+      vishwa.unfollow(archer)
+      assert_not vishwa.following?(archer)
+    end
+  end
+
+  test "feed should have the right posts" do
+    vishwa = users(:vishwa)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert vishwa.feed.include?(post_following)
+    end
+    # Posts from self
+    vishwa.microposts.each do |post_self|
+      assert vishwa.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+    assert_not vishwa.feed.include?(post_unfollowed)
+    end
+  end
 end
